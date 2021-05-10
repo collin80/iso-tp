32cad0fb75ff87baa51c6333b784b24c14b09f0b
Brandon Ros <brandonros1@gmail.com>
2021-05-10 19:47:03 -0400

    fix bug in send_ff path

--- src/iso-tp.cpp
+++ src/iso-tp.cpp
@@ -65,7 +65,8 @@ uint8_t IsoTp::send(Message_t* msg)
 #ifdef ISO_TP_DEBUG
                 Serial.println(F("Send FF"));
 #endif
-                if(!(retval=send_ff(msg)))   // FF complete
+                retval=send_ff(msg);
+                if(retval == 1)   // FF complete
                 {
                     msg->Buffer+=6;
                     msg->len-=6;
@@ -102,7 +103,7 @@ uint8_t IsoTp::send(Message_t* msg)
 #ifdef ISO_TP_DEBUG
             Serial.println(F("Send CF"));
 #endif
-            while(msg->len>7 & !bs)
+            while((msg->len > 7) & !bs)
             {
                 fc_delay(msg->min_sep_time);
                 if(!(retval=send_cf(msg)))
